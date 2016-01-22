c521fc2b7e653639bb2cb58ca9c908812b8eb20f
kevguy <kevlai@uw.edu>
2016-01-22 22:48:31 +0800

    Add code to get real data

---
+++ app/src/main/java/barqsoft/footballscores/widget/TodayWidgetIntentService.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package barqsoft.footballscores.widget;
+
+import android.annotation.TargetApi;
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.widget.RemoteViews;
+
+import java.sql.DatabaseMetaData;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import barqsoft.footballscores.MainActivity;
+import barqsoft.footballscores.R;
+import barqsoft.footballscores.Utilies;
+import barqsoft.footballscores.DatabaseContract;
+
+import barqsoft.footballscores.widget.TodayWidgetProvider;
+
+/**
+ * IntentService which handles updating all Today widgets with the latest data
+ */
+public class TodayWidgetIntentService extends IntentService {
+
+    private String[] fragmentdate = new String[1];
+
+    private static final String[] SCORE_COLUMNS = {
+            DatabaseContract.scores_table.MATCH_ID,
+            DatabaseContract.scores_table.DATE_COL,
+            DatabaseContract.scores_table.TIME_COL,
+            DatabaseContract.scores_table.HOME_COL,
+            DatabaseContract.scores_table.AWAY_COL,
+            DatabaseContract.scores_table.LEAGUE_COL,
+            DatabaseContract.scores_table.HOME_GOALS_COL,
+            DatabaseContract.scores_table.AWAY_GOALS_COL,
+            DatabaseContract.scores_table.MATCH_DAY
+    };
+    // these indices must match the projection
+    private static final int INDEX_MATCH_ID = 0;
+    private static final int INDEX_DATE = 1;
+    private static final int INDEX_TIME = 2;
+    private static final int INDEX_HOME = 3;
+    private static final int INDEX_AWAY = 4;
+    private static final int INDEX_LEAGUE = 5;
+    private static final int INDEX_HOME_GOALS = 6;
+    private static final int INDEX_AWAY_GOALS = 7;
+    private static final int INDEX_MATCH_DAY = 8;
+
+    public TodayWidgetIntentService() {
+        super("TodayWidgetIntentService");
+
+        int today = 0;
+        // FInd tomorrow's date
+        Date orgfragmentdate = new Date(System.currentTimeMillis()+((today+1)*86400000));
+        SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
+        setFragmentDate(mformat.format(orgfragmentdate));
+
+    }
+
+    public void setFragmentDate(String date)
+    {
+        fragmentdate[0] = date;
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // Retrieve all of the Today widget ids: these are the widgets we need to update
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this,
+                TodayWidgetProvider.class));
+
+        // Get tomorrow's data from the ContentProvider
+        Uri scoreForTomorrowUri = DatabaseContract.scores_table.buildScoreWithDate();
+        Cursor data = getContentResolver().query(scoreForTomorrowUri, SCORE_COLUMNS, null, fragmentdate, null);
+        if (data == null) {
+            return;
+        }
+        if (!data.moveToFirst()) {
+            data.close();
+            return;
+        }
+
+        // Extract the score data from the Cursor
+        int scoreId = data.getInt(INDEX_MATCH_ID);
+        int scoreArtResourceId = R.drawable.liverpool;
+        String description = "fuck";
+        String homeTeam = data.getString(INDEX_HOME);
+        data.close();
+
+        // Perform this loop procedure for each Today widget
+        for (int appWidgetId : appWidgetIds) {
+            int layoutId = R.layout.widget_today_small;
+            RemoteViews views = new RemoteViews(getPackageName(), layoutId);
+
+            // Add the data to the RemoteViews
+            views.setImageViewResource(R.id.widget_icon, scoreArtResourceId);
+            // Content Descriptions for RemoteViews were only added in ICS MR1
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                setRemoteContentDescription(views, description);
+            }
+            views.setTextViewText(R.id.widget_high_temperature, homeTeam);
+
+            // Create an Intent to launch MainActivity
+            Intent launchIntent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+
+        /*
+        String location = Utility.getPreferredLocation(this);
+        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                location, System.currentTimeMillis());
+        Cursor data = getContentResolver().query(weatherForLocationUri, SCORE_COLUMNS, null,
+                null, WeatherContract.WeatherEntry.COLUMN_DATE + " ASC");
+        if (data == null) {
+            return;
+        }
+        if (!data.moveToFirst()) {
+            data.close();
+            return;
+        }
+
+
+        // Extract the weather data from the Cursor
+        int weatherId = data.getInt(INDEX_WEATHER_ID);
+        int weatherArtResourceId = Utility.getArtResourceForWeatherCondition(weatherId);
+        String description = data.getString(INDEX_SHORT_DESC);
+        double maxTemp = data.getDouble(INDEX_MAX_TEMP);
+        String formattedMaxTemperature = Utility.formatTemperature(this, maxTemp);
+        data.close();
+
+
+
+        // Perform this loop procedure for each Today widget
+        for (int appWidgetId : appWidgetIds) {
+            int layoutId = R.layout.widget_today_small;
+            RemoteViews views = new RemoteViews(getPackageName(), layoutId);
+
+            // Add the data to the RemoteViews
+            views.setImageViewResource(R.id.widget_icon, weatherArtResourceId);
+            // Content Descriptions for RemoteViews were only added in ICS MR1
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                setRemoteContentDescription(views, description);
+            }
+            views.setTextViewText(R.id.widget_high_temperature, formattedMaxTemperature);
+
+            // Create an Intent to launch MainActivity
+            Intent launchIntent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+        */
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+    private void setRemoteContentDescription(RemoteViews views, String description) {
+        views.setContentDescription(R.id.widget_icon, description);
+    }
+}
--- app/src/main/java/barqsoft/footballscores/widget/TodayWidgetProvider.java
+++ app/src/main/java/barqsoft/footballscores/widget/TodayWidgetProvider.java
@@ -22,48 +22,40 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.widget.RemoteViews;
 
 import barqsoft.footballscores.MainActivity;
 import barqsoft.footballscores.R;
 import barqsoft.footballscores.Utilies;
+import barqsoft.footballscores.service.myFetchService;
 
 
 /**
- * Provider for a widget showing today's weather.
+ * Provider for a widget showing today's weather
+ *
+ * Delegates widget updating to {@link TodayWidgetIntentService} to ensure that
+ * data retrieval is done on a background thread.
  */
 public class TodayWidgetProvider extends AppWidgetProvider {
 
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        int scoreArtResourceId = R.drawable.liverpool;
-        String description = "Clear";
-        String score = "fuck";
-
-        // Perform this loop procedure for each Today widget
-        for (int appWidgetId : appWidgetIds) {
-            int layoutId = R.layout.widget_today_small;
-            RemoteViews views = new RemoteViews(context.getPackageName(), layoutId);
-
-            // Add the data to the RemoteViews
-            views.setImageViewResource(R.id.widget_icon, scoreArtResourceId);
-            // Content Descriptions for RemoteViews were only added in ICS MR1
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
-                setRemoteContentDescription(views, description);
-            }
-            views.setTextViewText(R.id.widget_high_temperature, score);
-
-            // Create an Intent to launch MainActivity
-            Intent launchIntent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
-            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
-
-            // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
     }
 
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
-    private void setRemoteContentDescription(RemoteViews views, String description) {
-        views.setContentDescription(R.id.widget_icon, description);
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
+                                          int appWidgetId, Bundle newOptions) {
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
+    }
+
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+        if (myFetchService.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            context.startService(new Intent(context, TodayWidgetIntentService.class));
+        }
     }
 }
